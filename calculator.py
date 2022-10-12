1177478f35b22ac3f0913bbd3887de7d56b3f1bb
pntnguyenthao <theronpham1510@gmail.com>
2022-10-12 17:59:14 -0500

    create function division

--- calculator.py
+++ calculator.py
@@ -3,5 +3,8 @@ class Calculator:
 		return n1 + n2
 	def substract(n1, n2):
 		return n1 - n2;
-	def multiply (n1, n2);
+	def multiply (n1, n2):
 		return (n1 * n2);
+	def division (n1, n2):
+		return (n1 / n2);
+This is to test my understanding
